456eafff9058eefbcc7664e48d00f8d791c17ad2
eHutchful <ehprimus09@gmail.com>
2016-07-15 19:05:26 +0000

    MPower initiated

---
+++ 10K 5/eTransact.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="eTransact.aspx.cs" Inherits="_10K_5.eTransact" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 10K 5/eTransact.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MPowerPayments;
+
+namespace _10K_5
+{
+    public partial class eTransact : System.Web.UI.Page
+    {
+        MPowerSetup setup = new MPowerSetup()
+        {
+            MasterKey = "dd6f2c90-f075-012f-5b69-00155d864600",
+            PrivateKey = "test_private_oDLVlm1eNyh0IsetdhdJvcl0ygA",
+            PublicKey = "test_public_zzF3ywvX9DE-OSDNhUqKoaTI4wc",
+            Token = "ca03737cf942cf644f36",
+            Mode = "test"
+        };
+
+        MPowerStore store = new MPowerStore()
+        {
+            Name = "10KNetwork Store",
+            Tagline = "This is my awesome tagline",
+            PhoneNumber = "030200001",
+            PostalAddress = "P. O. Box 10770 Accra North Ghana",
+            LogoUrl = "http://www.mylogourl.com/photo.png"
+        };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+           
+
+        // If you intend to use a simpler approach by redirecting to the MPower checkout page
+        // Please note that the constructor requires a instance of both
+        // MPowerSetup & MPowerStore Classes respectively
+        MPowerCheckoutInvoice invoice = new MPowerCheckoutInvoice(setup, store)
+    {
+       MPowerstore.Name = "10K Network"
+    };
+
+        // If you wish to accept payments directly on your service
+        // Please note that the constructor requires a instance of both
+        // MPowerSetup & MPowerStore Classes respectively
+        MPowerOnsiteInvoice invoice = new MPowerOnsiteInvoice(setup, store);
+    }
+}
